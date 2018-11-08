53a6ac656a3238c9c6d420c322773bd6c1a1f78b
Nolan Allen <nolan.r.allen@gmail.com>
2018-11-08 12:28:30 -0600

    Homepage desktop and mobile complete.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // JS goes here
+
+const navButtons = document.querySelector('.hidden-menu');
+const hamburger = document.querySelector('.hamburger');
+const hamburgerOpen = document.querySelector('#open-hamburger');
+const hamburgerClose = document.querySelector('#closed-hamburger');
+
+hamburger.addEventListener('click', function() {
+    navButtons.classList.toggle('hidden');
+    hamburgerOpen.classList.toggle('hidden');
+    hamburgerClose.classList.toggle('hidden');
+})
